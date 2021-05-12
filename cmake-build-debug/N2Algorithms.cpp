f30a011270821abd5952bcdeb96a3731ed771655
Ahmed Ossama <58595131+AhmedOssama22dev@users.noreply.github.com>
2021-05-12 05:46:01 +0200

    Selection Sort added

--- cmake-build-debug/N2Algorithms.cpp
+++ cmake-build-debug/N2Algorithms.cpp
@@ -3,9 +3,21 @@
 //
 
 #include "N2Algorithms.h"
+#include <algorithm>
 
 void N2Algorithms::selectionSort(int *arr, int n) {
     // TODO: AHMED OSSAMA TASK
+    int min_index;
+    for(int i = 0; i<n-1; i++)
+    {
+        min_index = i;
+        for(int j = i+1; j<n; j++)
+        {
+            if(arr[j]<arr[min_index])
+                min_index = j;
+        }
+        swap(arr[i],arr[min_index])
+    }
 }
 
 void N2Algorithms::bubbleSort(int *arr, int n) {
