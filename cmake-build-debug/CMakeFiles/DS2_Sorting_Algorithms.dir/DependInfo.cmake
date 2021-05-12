396c9169187c8fe30559e2b7f3e88e8186be32c3
Youssef <developer.iyoussef@hotmail.com>
2021-05-12 04:09:46 +0200

    Initial Commit

---
+++ cmake-build-debug/CMakeFiles/DS2_Sorting_Algorithms.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Youssef/CLionProjects/DS2 Sorting Algorithms/main.cpp" "/Users/Youssef/CLionProjects/DS2 Sorting Algorithms/cmake-build-debug/CMakeFiles/DS2_Sorting_Algorithms.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/DS2_Sorting_Algorithms.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DS2_Sorting_Algorithms.dir/main.cpp.o"
+  "DS2_Sorting_Algorithms"
+  "DS2_Sorting_Algorithms.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DS2_Sorting_Algorithms.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
