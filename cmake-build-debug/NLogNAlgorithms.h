ee1896a04145a89fe138add7881d06032191df8d
Rwan sobhy <75646701+rwansobhy@users.noreply.github.com>
2021-05-12 07:06:34 +0200

    Update NLogNAlgorithms.h

--- cmake-build-debug/NLogNAlgorithms.h
+++ cmake-build-debug/NLogNAlgorithms.h
@@ -12,7 +12,7 @@ class NLogNAlgorithms {
 private:
     static void heapify(int *arr, int n, int i);
 public:
-    static void mergeSort(int *arr, int n);
+    static void mergeSort(int *arr,unsigned int low,unsigned int high);
     static void heapSort(int *arr, int n);
     static void quickSort(int *arr, int n);
 };
