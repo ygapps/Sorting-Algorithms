17b981dfa5b1bb7e1ac643cef9939dd71dd6dd66
Rwan sobhy <75646701+rwansobhy@users.noreply.github.com>
2021-05-12 06:58:03 +0200

    Update Utility.cpp

--- cmake-build-debug/Utility.cpp
+++ cmake-build-debug/Utility.cpp
@@ -1,7 +1,9 @@
 //
 // Created by Youssef on 5/12/21.
 //
-
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
 #include "Utility.h"
 
 void Utility::displayArray(int *arr, int n) {
@@ -9,4 +11,19 @@ void Utility::displayArray(int *arr, int n) {
     for (i = 0; i < n; i++)
         std::cout << arr[i] << " ";
     std::cout << std::endl;
+}
+
+
+int Utility:: randomArrayGenerator()
+{
+   unsigned int sizeArray[5]={10,100,1000,10000,100000};
+    srand ( time(NULL) ); //initialize the random seed
+    int RandIndex = rand() % 5; //generates a random number between 0 and 4
+    int randArray[sizeArray[RandIndex]];
+    for(unsigned int i=0;i<sizeArray[RandIndex];i++)
+    {
+        randArray[i]=rand();
+    }
+
+  return *randArray;
 }
